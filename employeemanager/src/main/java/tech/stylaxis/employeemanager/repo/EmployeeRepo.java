2438abdd4ea67c5c75783cd9e42dae8d85adc488
sameerapeiris <sameedsdp@gmail.com>
2024-02-29 09:59:00 +0200

    employee manager angular

---
+++ employeemanager/src/main/java/tech/stylaxis/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,14 @@
+package tech.stylaxis.employeemanager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.stylaxis.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+
+
+}
